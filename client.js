1899f17b5e85a3b7f73daea474bf688ce1f29988
Tien Nguyen <115622012+skullick@users.noreply.github.com>
2025-06-03 16:22:47 +0700

    Update client.js

--- client.js
+++ client.js
@@ -46,10 +46,6 @@ function start() {
         sdpSemantics: 'unified-plan'
     };
 
-    if (document.getElementById('use-stun').checked) {
-        config.iceServers = [{urls: ['stun:stun.l.google.com:19302']}];
-    }
-
     pc = new RTCPeerConnection(config);
 
     // connect audio / video
